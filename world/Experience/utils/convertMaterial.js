aa33e2bc5c251c85c814f068f7bd57ae88cdc680
rajvveer <rajveershekhawat626@gmail.com>
2026-09-06 01:23:53 +0530

    perf: reuse baked materials and transparent water surfaces

---
+++ world/Experience/utils/convertMaterial.js
@@ -0,0 +1,25 @@
+import { MeshBasicMaterial } from "three";
+
+export const convertMaterialsToMeshBasicMaterial = (
+  materials,
+  alphaTestValue = 0.55
+) => {
+  Object.keys(materials).forEach((materialKey) => {
+    const material = materials[materialKey];
+    if (material.isMeshBasicMaterial) return;
+
+    if (material.emissiveMap) {
+      materials[materialKey] = new MeshBasicMaterial({
+        map: material.emissiveMap,
+      });
+    } else {
+      materials[materialKey] = new MeshBasicMaterial({
+        map: material.map,
+        transparent: true,
+        alphaTest: alphaTestValue,
+      });
+    }
+  });
+
+  return materials;
+};
